8cd1396a55775b2519f92850d94a79fb8744db27
Youssef <youssefeltoukhi@hotmail.com>
2023-06-18 18:33:39 +0400

    minimap now displays player

--- draw_minimap.c
+++ draw_minimap.c
@@ -7,48 +7,69 @@ int get_color(char c) {
     return (WHITE);
 }
 
-void	draw_square(t_data *data, int x, int y, int color)
+void	draw_square(t_data *data, t_vector pos, size_t size, int color)
 {
-	int	a;
-	int i;
-	int j;
+	size_t i;
+	size_t j;
 
-	a = 15;
 	i = 0;
-	while (i < a)
+	while (i < size)
 	{
 		j = 0;
-		while (j++ < a)
+		while (j++ < size)
 		{
-			my_mlx_pixel_put(data, x, y, color);
-			x++;
+			my_mlx_pixel_put(data, pos.x, pos.y, color);
+			pos.x++;
 		}
-		x -= j - 1;
+		pos.x -= j - 1;
 		i++;
-		y++;
+		pos.y++;
 	}
 }
 
+// void	draw_circle(t_data *data, int x, int y, int color)
+// {
+// 	int radius;
+// 	double PI;
+// 	double angle;
+
+// 	PI = 3.1415926535;
+// 	radius = 4;
+// 	while (radius > 0)
+// 	{
+// 		angle = 0;
+// 		while (angle < 360)
+// 		{
+// 			my_mlx_pixel_put(data, x + radius * cos(angle * PI / 180), 
+// 								y + radius * sin(angle * PI / 180), color);
+// 			angle += 0.1;
+// 		}
+// 		radius--;
+// 	}
+	
+// }
+
 void	draw_minimap(t_vars *vars)
 {
-	size_t	i;
-	size_t	j;
-    int x;
-    int y;
+	size_t		i;
+	size_t		j;
+    t_vector	pos;
 
 	i = 0;
-    y = 0;
+    pos.y = 0;
 	while (i < vars->scene.size)
 	{
-        x = 0;
+        pos.x = 0;
 		j = 0;
 		while (j < ft_strlen(vars->scene.minimap[i]))
 		{
-            draw_square(&vars->image, x, y, get_color(vars->scene.minimap[i][j]));
-            x += 15;
+            draw_square(&vars->image, pos, 15, get_color(vars->scene.minimap[i][j]));
+            pos.x += 15;
             j++;
 		}
 		i++;
-        y += 15;
+        pos.y += 15;
 	}
+	draw_square(&vars->image, vars->player.pos, 3, RED);
+	// draw_circle(&vars->image, (vars->player.pos.x + 1) * 15, (vars->player.pos.y + 1) * 15, RED);
 }
