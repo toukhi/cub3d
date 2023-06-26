1928fd55ba07cd148a1b3559883d65b8ccc958d1
Youssef <youssefeltoukhi@hotmail.com>
2023-06-26 14:12:10 +0400

    Implemented DDA to know when rays hit the wall

--- rays.c
+++ rays.c
@@ -1,12 +1,22 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rays.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-touk <yel-touk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 13:32:08 by yel-touk          #+#    #+#             */
+/*   Updated: 2023/06/26 14:07:13 by yel-touk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "cub3D.h"
 
 void	init_rays(t_vars *vars)
 {
-	int	ray_num;
+	int		ray_num;
 	int		w;
-	double cameraX;
+	double	cameraX;
 	t_ray	current_ray;
 
 	w = vars->scene.longest;
@@ -19,6 +29,7 @@ void	init_rays(t_vars *vars)
 		current_ray = vars->rays[ray_num];
 		current_ray.mapX = (int) vars->player.pos.x;
 		current_ray.mapY = (int) vars->player.pos.y;
+		// printf("mapX: %d, mapY: %d\n", current_ray.mapX, current_ray.mapY);
 		cameraX = 2 * ray_num / (double) w - 1;
 		current_ray.dir.x = vars->player.dir.x + vars->player.plane.x * cameraX;
 		current_ray.dir.y = vars->player.dir.y + vars->player.plane.y * cameraX;
@@ -48,7 +59,31 @@ void	init_rays(t_vars *vars)
 			current_ray.step.y = 1;
 			current_ray.sideDist.y = (current_ray.mapY + 1.0 - vars->player.pos.y) * current_ray.deltaDist.y;
 		}
-		printf("For ray %d: sideDistX = %f, sideDistY = %f\n", ray_num, current_ray.sideDist.x, vars->rays[ray_num].sideDist.y);
+		while (current_ray.hit == 0)
+		{
+			printf("For ray %d: deltaDX = %f, deltaDY = %f, sideDX = %f, sideDY = %f\n", ray_num, current_ray.deltaDist.x, current_ray.deltaDist.y, current_ray.sideDist.x, current_ray.sideDist.y);
+			//jump to next map square, either in x-direction, or in y-direction
+			if (current_ray.sideDist.x < current_ray.sideDist.y)
+			{
+				current_ray.sideDist.x += current_ray.deltaDist.x;
+				current_ray.mapX += current_ray.step.x;
+				current_ray.side = 0;
+			}
+			else
+			{
+				current_ray.sideDist.y += current_ray.deltaDist.y;
+				current_ray.mapY += current_ray.step.y;
+				current_ray.side = 0;
+			}
+			//Check if ray has hit a wall
+			// printf("current content: %c\n", vars->scene.content[current_ray.mapY][current_ray.mapX]);
+			if (vars->scene.content[current_ray.mapY][current_ray.mapX] > '0')
+			{
+				printf("mapX: %d, mapY: %d\n", current_ray.mapX, current_ray.mapY);
+				current_ray.hit = 1;
+			}
+		}
+		printf("For ray %d: deltaDX = %f, deltaDY = %f, sideDX = %f, sideDY = %f\n", ray_num, current_ray.deltaDist.x, current_ray.deltaDist.y, current_ray.sideDist.x, current_ray.sideDist.y);
 	}
 	// printf("longest: %d\n", w);
 }
