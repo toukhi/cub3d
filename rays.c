330a75dc7e4443df9251ae639692ef6006fc8065
Youssef El Toukhi <youssefeltoukhi@hotmail.com>
2023-06-27 17:30:03 +0400

    Code is now more readable

--- rays.c
+++ rays.c
@@ -3,21 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   rays.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-touk <yel-touk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youssef <youssef@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 13:32:08 by yel-touk          #+#    #+#             */
-/*   Updated: 2023/06/26 14:24:17 by yel-touk         ###   ########.fr       */
+/*   Updated: 2023/06/27 17:28:59 by youssef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+void    set_sideDist(t_ray *ray_p, t_player player)
+{
+    t_ray   ray;
+
+    ray = *ray_p;
+    if (ray.dir.x < 0)
+	{
+		(*ray_p).step.x = -1;
+		(*ray_p).sideDist.x = (player.pos.x - ray.mapX) * ray.deltaDist.x;
+	}
+	else
+	{
+		(*ray_p).step.x = 1;
+		(*ray_p).sideDist.x = (ray.mapX + 1.0 - player.pos.x) * ray.deltaDist.x;
+	}
+	if (ray.dir.y < 0)
+	{
+		(*ray_p).step.y = -1;
+		(*ray_p).sideDist.y = (player.pos.y - ray.mapY) * ray.deltaDist.y;
+	}
+	else
+	{
+		(*ray_p).step.y = 1;
+		(*ray_p).sideDist.y = (ray.mapY + 1.0 - player.pos.y) * ray.deltaDist.y;
+	}
+}
+
+void    dda(t_ray *ray_p, char **map, int ray_num)
+{
+    while ((*ray_p).hit == 0)
+	{
+		printf("For ray %d: deltaDX = %f, deltaDY = %f, sideDX = %f, sideDY = %f\n", ray_num, (*ray_p).deltaDist.x, (*ray_p).deltaDist.y, (*ray_p).sideDist.x, (*ray_p).sideDist.y);
+		//jump to next map square, either in x-direction, or in y-direction
+		if ((*ray_p).sideDist.x < (*ray_p).sideDist.y)
+		{
+			(*ray_p).sideDist.x += (*ray_p).deltaDist.x;
+			(*ray_p).mapX += (*ray_p).step.x;
+			(*ray_p).side = 0;
+		}
+		else
+		{
+			(*ray_p).sideDist.y += (*ray_p).deltaDist.y;
+			(*ray_p).mapY += (*ray_p).step.y;
+			(*ray_p).side = 0;
+		}
+		//Check if ray has hit a wall
+		// printf("current content: %c\n", map[(*ray_p).mapY][(*ray_p).mapX]);
+		if (map[(*ray_p).mapY][(*ray_p).mapX] > '0')
+		{
+			printf("mapX: %d, mapY: %d\n", (*ray_p).mapX, (*ray_p).mapY);
+			(*ray_p).hit = 1;
+		}
+	}
+}
 
 void	init_rays(t_vars *vars)
 {
 	int		ray_num;
 	int		w;
 	double	cameraX;
-	t_ray	current_ray;
+	t_ray	*ray_p;
 
 	w = vars->scene.longest;
 	vars->rays = malloc((w + 1) * sizeof(t_ray));
@@ -26,69 +81,27 @@ void	init_rays(t_vars *vars)
 	ray_num = -1;
 	while (++ray_num <= w)
 	{
-		current_ray = vars->rays[ray_num];
-		current_ray.mapX = (int) vars->player.pos.x;
-		current_ray.mapY = (int) vars->player.pos.y;
-		// printf("mapX: %d, mapY: %d\n", current_ray.mapX, current_ray.mapY);
+		ray_p = &(vars->rays[ray_num]);
+		(*ray_p).mapX = (int) vars->player.pos.x;
+		(*ray_p).mapY = (int) vars->player.pos.y;
+		// printf("mapX: %d, mapY: %d\n", (*ray_p).mapX, (*ray_p).mapY);
 		cameraX = 2 * ray_num / (double) w - 1;
-		current_ray.dir.x = vars->player.dir.x + vars->player.plane.x * cameraX;
-		current_ray.dir.y = vars->player.dir.y + vars->player.plane.y * cameraX;
-		//need to check if rayDir is 0!!
-		current_ray.deltaDist.x = fabs(1 / current_ray.dir.x);
-		current_ray.deltaDist.y = fabs(1 / current_ray.dir.y);
-		// printf("For ray %d: deltaDistX = %f, deltaDistY = %f\n", ray_num, current_ray.deltaDist.x, vars->rays[ray_num].deltaDist.y);
-		// printf("For ray %d: dirX = %f, dirY = %f\n", ray_num, current_ray.dir.x, current_ray.dir.y);
-		current_ray.hit = 0;
-		if (current_ray.dir.x < 0)
-		{
-			current_ray.step.x = -1;
-			current_ray.sideDist.x = (vars->player.pos.x - current_ray.mapX) * current_ray.deltaDist.x;
-		}
+		(*ray_p).dir.x = vars->player.dir.x + vars->player.plane.x * cameraX;
+		(*ray_p).dir.y = vars->player.dir.y + vars->player.plane.y * cameraX;
+		//need to check if rayDir is 0!!!
+		(*ray_p).deltaDist.x = fabs(1 / (*ray_p).dir.x);
+		(*ray_p).deltaDist.y = fabs(1 / (*ray_p).dir.y);
+		// printf("For ray %d: deltaDistX = %f, deltaDistY = %f\n", ray_num, (*ray_p).deltaDist.x, vars->rays[ray_num].deltaDist.y);
+		// printf("For ray %d: dirX = %f, dirY = %f\n", ray_num, (*ray_p).dir.x, (*ray_p).dir.y);
+		(*ray_p).hit = 0;
+		set_sideDist(ray_p, vars->player);
+		dda(ray_p, vars->scene.content, ray_num);
+		// printf("For ray %d: deltaDX = %f, deltaDY = %f, sideDX = %f, sideDY = %f\n", ray_num, (*ray_p).deltaDist.x, (*ray_p).deltaDist.y, (*ray_p).sideDist.x, (*ray_p).sideDist.y);
+		if ((*ray_p).side == 0)
+			(*ray_p).wallDist = ((*ray_p).sideDist.x - (*ray_p).deltaDist.x);
 		else
-		{
-			current_ray.step.x = 1;
-			current_ray.sideDist.x = (current_ray.mapX + 1.0 - vars->player.pos.x) * current_ray.deltaDist.x;
-		}
-		if (current_ray.dir.y < 0)
-		{
-			current_ray.step.y = -1;
-			current_ray.sideDist.y = (vars->player.pos.y - current_ray.mapY) * current_ray.deltaDist.y;
-		}
-		else
-		{
-			current_ray.step.y = 1;
-			current_ray.sideDist.y = (current_ray.mapY + 1.0 - vars->player.pos.y) * current_ray.deltaDist.y;
-		}
-		while (current_ray.hit == 0)
-		{
-			printf("For ray %d: deltaDX = %f, deltaDY = %f, sideDX = %f, sideDY = %f\n", ray_num, current_ray.deltaDist.x, current_ray.deltaDist.y, current_ray.sideDist.x, current_ray.sideDist.y);
-			//jump to next map square, either in x-direction, or in y-direction
-			if (current_ray.sideDist.x < current_ray.sideDist.y)
-			{
-				current_ray.sideDist.x += current_ray.deltaDist.x;
-				current_ray.mapX += current_ray.step.x;
-				current_ray.side = 0;
-			}
-			else
-			{
-				current_ray.sideDist.y += current_ray.deltaDist.y;
-				current_ray.mapY += current_ray.step.y;
-				current_ray.side = 0;
-			}
-			//Check if ray has hit a wall
-			// printf("current content: %c\n", vars->scene.content[current_ray.mapY][current_ray.mapX]);
-			if (vars->scene.content[current_ray.mapY][current_ray.mapX] > '0')
-			{
-				printf("mapX: %d, mapY: %d\n", current_ray.mapX, current_ray.mapY);
-				current_ray.hit = 1;
-			}
-		}
-		// printf("For ray %d: deltaDX = %f, deltaDY = %f, sideDX = %f, sideDY = %f\n", ray_num, current_ray.deltaDist.x, current_ray.deltaDist.y, current_ray.sideDist.x, current_ray.sideDist.y);
-		if (current_ray.side == 0)
-			current_ray.wallDist = (current_ray.sideDist.x - current_ray.deltaDist.x);
-		else
-			current_ray.wallDist = (current_ray.sideDist.y - current_ray.deltaDist.y);
-		printf("For ray %d: distance to wall is: %f\n\n", ray_num, current_ray.wallDist);
+			(*ray_p).wallDist = ((*ray_p).sideDist.y - (*ray_p).deltaDist.y);
+		printf("For ray %d: distance to wall is: %f\n\n", ray_num, (*ray_p).wallDist);
 	}
 	// printf("longest: %d\n", w);
 }
