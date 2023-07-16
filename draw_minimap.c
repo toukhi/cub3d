8d3aead1c85e532d4f35096d0711492ec604adae
Youssef El Toukhi <youssefeltoukhi@hotmail.com>
2023-07-17 00:13:45 +0400

    Fixed minimap and norm errors

--- draw_minimap.c
+++ draw_minimap.c
@@ -6,7 +6,7 @@
 /*   By: youssef <youssef@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 14:24:48 by yel-touk          #+#    #+#             */
-/*   Updated: 2023/07/16 18:50:23 by youssef          ###   ########.fr       */
+/*   Updated: 2023/07/17 00:13:17 by youssef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,75 +45,51 @@ void	player_to_pixel(t_vector pos, t_vector *pixel)
 	pixel->y = pos.y * BLOCK_SIZE;
 }
 
-void	draw_minimap(t_vars *vars)
+void	draw_minimap2(t_vars *vars, t_vector *pixel,
+						t_vector *pos, t_vector *step)
 {
-	int			i;
-	int			j;
-	t_vector	pixel_pos;
-	int			a;
-	int			b;
-	int			tmp_a;
-	int			tmp_b;
-	int block_size;
+	int	x;
+	int	y;
+	int	color;
 
-	block_size = BLOCK_SIZE;
-	if (vars->scene.size < MAP_HEIGHT / BLOCK_SIZE)
-		block_size = MAP_HEIGHT / vars->scene.size;
-	i = 0;
-	pixel_pos.y = MAP_PADDING;
-	a = vars->player.pos.y -  MAP_HEIGHT / block_size / 2;
-	while (i < MAP_HEIGHT / block_size && i < (int)vars->scene.size)
+	pixel->y = MAP_PADDING;
+	pos->y = vars->player.pos.y - (double)MAP_HEIGHT / 2 / BLOCK_SIZE;
+	step->y = (vars->player.pos.y - pos->y) / ((double)MAP_HEIGHT / 2);
+	x = (int)floor(pos->x);
+	if (pos->x < 0)
+		x = 0;
+	else if (pos->x >= vars->scene.longest)
+		x = (int) vars->scene.longest - 1;
+	while (pixel->y < MAP_HEIGHT + MAP_PADDING)
 	{
-		pixel_pos.x = MAP_PADDING;
-		j = 0;
-		b = vars->player.pos.x -  MAP_WIDTH / block_size / 2;
-		tmp_a = a;
-		if (a < 0)
-			tmp_a = 0;
-		if (a >= (int) vars->scene.size)
-			tmp_a = vars->scene.size -1;
-		while (j < MAP_WIDTH / block_size)
-		{
-			tmp_b = b;
-			if (b < 0)
-				tmp_b = 0;
-			if (tmp_b >= (int) ft_strlen(vars->scene.minimap[i]))
-				tmp_b = (int) ft_strlen(vars->scene.minimap[i]) - 1;
-			draw_square(&vars->image, pixel_pos, block_size, get_color(vars->scene.minimap[tmp_a][tmp_b]));
-			pixel_pos.x += block_size;
-			j++;
-			b++;
-		}
-		i++;
-		a++;
-		pixel_pos.y += block_size;
+		y = (int)floor(pos->y);
+		if (pos->y < 0)
+			y = 0;
+		else if (pos->y >= (int)vars->scene.size)
+			y = (int) vars->scene.size - 1;
+		color = get_color(vars->scene.minimap[y][x]);
+		my_mlx_pixel_put(&vars->image, pixel->x, pixel->y, color);
+		pixel->y++;
+		pos->y += step->y;
 	}
-	pixel_pos.x = MAP_WIDTH / 2;
-	pixel_pos.y = MAP_HEIGHT / 2;
-	draw_square(&vars->image, pixel_pos, 4, RED);
 }
 
-// void	draw_minimap(t_vars *vars)
-// {
-// 	size_t		i;
-// 	size_t		j;
-//     t_vector	pixel_pos;
-	
-// 	i = 0;
-//     pixel_pos.y = 0;
-// 	while (i < vars->scene.size && i < WIN_HEIGHT - BLOCK_SIZE)
-// 	{
-//         pixel_pos.x = 0;
-// 		j = 0;
-// 		while (j < ft_strlen(vars->scene.minimap[i]) )//&& j < WIN_WIDTH - BLOCK_SIZE)
-// 		{
-//             draw_square(&vars->image, pixel_pos, BLOCK_SIZE, get_color(vars->scene.minimap[i][j]));
-//             pixel_pos.x += BLOCK_SIZE;
-//             j++;
-// 		}
-// 		i++;
-//         pixel_pos.y += BLOCK_SIZE;
-// 	}
-// 	player_to_pixel(vars->player.pos, &pixel_pos);
-// 	draw_square(&vars->image, pixel_pos, 3, RED);
-// }
+void	draw_minimap(t_vars *vars)
+{
+	t_vector	pixel;
+	t_vector	pos;
+	t_vector	step;
+
+	pixel.x = MAP_PADDING;
+	pos.x = vars->player.pos.x - (double)MAP_WIDTH / BLOCK_SIZE / 2 ;
+	step.x = (vars->player.pos.x - pos.x) / ((double)MAP_WIDTH / 2);
+	while (pixel.x < MAP_WIDTH + MAP_PADDING)
+	{
+		draw_minimap2(vars, &pixel, &pos, &step);
+		pixel.x++;
+		pos.x += step.x;
+	}
+	pixel.x = MAP_WIDTH / 2 + MAP_PADDING;
+	pixel.y = MAP_HEIGHT / 2 + MAP_PADDING;
+	draw_square(&vars->image, pixel, 4, RED);
+}
